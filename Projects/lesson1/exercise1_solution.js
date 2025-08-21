d7237e6ed9fff99aa708801a26e8ec8c7b0707ea
AarthiyaMuthusamy <aarthiyam1510@gmail.com>
2025-08-21 12:36:59 +0530

    Numbers and Math completed with projects

---
+++ Projects/lesson1/exercise1_solution.js
@@ -0,0 +1,16 @@
+
+alert('Good Morning!');
+alert('Aarthiya');
+10+5
+20-5
+2+2-5
+document.body.innerHTML = 'Good Morning';
+document.body.innerHTML = 'Aarthiya';
+10 + 8 + 20
+100 -20 - 50 + 200
+document.body.innerHTML = '';
+
+
+
+
+
