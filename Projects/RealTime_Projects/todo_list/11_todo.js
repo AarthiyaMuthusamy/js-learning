02cfe282dbe87ae2dbc844956ae032e478443f22
AarthiyaMuthusamy <aarthiyam1510@gmail.com>
2025-09-09 12:35:33 +0530

    lesson 11 part 2 Arrays and loops concepts are done

--- Projects/RealTime_Projects/todo_list/11_todo.js
+++ Projects/RealTime_Projects/todo_list/11_todo.js
@@ -1,20 +1,50 @@
-const todoList = [];
+const todoList = JSON.parse(localStorage.getItem('todoList')) || [
+  { 
+  name: 'make dinner',
+  dueDate: '2025-12-23'
+  },
+  { 
+  name: 'wash clothes',
+  dueDate: '2025-12-22'
+  }
+ 
+];
 
 rendertodo();
-
+ 
 function rendertodo(){
-  let todoListHtml = '';
+
+
+   let todoListHtml = '';
+
+   
 
   for (let i = 0; i < todoList.length; i++) {
-    const todo = todoList[i];
-    const html = `<p>${todo}</p>`;
+    const todoObject = todoList[i];
+    const {name , dueDate} = todoObject;
+   
+
+    const html = `
+    <div>${name}</div>
+    <div>${dueDate}</div>
+    
+    <button onclick="
+    todoList.splice(${i},1);
+     rendertodo();
+     saveToStorage();
+    " class ="del-button">Delete</button>`;
+
     todoListHtml += html;
+
+    
     
   }
+  
+   document.querySelector('.js-todo-list').innerHTML = todoListHtml;
+  
+ 
 
-  console.log(todoListHtml);
-
-  document.querySelector('.js-todo-list').innerHTML = todoListHtml;
+  
 }
 
 
@@ -22,12 +52,26 @@ function rendertodo(){
 function addTodo(){
   const inputElement = document.querySelector('.js-name-input');
   const name = inputElement.value;
+  const dateInputElement =  document.querySelector('.js-due-date-input');
+  const dueDate= dateInputElement.value;
   
 
-  todoList.push(name);
-  console.log(todoList);
+  todoList.push({
+  
+    name,
+    dueDate
+  });
+  
 
   inputElement.value = ''; //make the text will be empty
+  dateInputElement.value = '';
 
   rendertodo();
+  saveToStorage();
+ 
+}
+
+
+function saveToStorage(){
+   localStorage.setItem('todoList',JSON.stringify(todoList));
 }
